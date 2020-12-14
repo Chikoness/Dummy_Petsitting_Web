d4a5a547f8b81a0872d0af3292b7dcb3d501aa0a
Charlene A <chichiko90@gmail.com>
2020-12-15 00:54:50 +0800

    Services cards

--- petsitting-gatsby/src/pages/index.js
+++ petsitting-gatsby/src/pages/index.js
@@ -3,11 +3,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
+import Services from "../components/services"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <Services heading="What We Offer"/>
   </Layout>
 )
 
